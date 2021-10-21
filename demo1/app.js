9b11770fc9e12dc60d97d898405c576e2f0b4809
santhoshkumar-27 <it.santhoshkumar@outlook.com>
2021-10-21 19:10:19 +0530

    added

---
+++ demo1/app.js
@@ -0,0 +1,11 @@
+let name1 = document.getElementById("name");
+document.getElementsByClassName('output')[1].innerHTML = "This is very dangerous hacker's " + name1.innerHTML;
+
+// document.getElementById('date').innerHTML = 'this is very i want';
+document.getElementsByTagName('h1').style.color = pink;
+
+
+function myNumber(){
+    let string = 'stings1';
+    document.getElementById('std').innerHTML = string.innerHTML;
+}
